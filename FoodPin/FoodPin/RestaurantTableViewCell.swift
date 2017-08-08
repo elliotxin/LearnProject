40beb85499a841cc8b76d1f922743e22ae5875a3
elliot xin <elliotxin@gmail.com>
2017-08-08 15:37:31 +0800

    add one model for the tableView Cell. Customize tableView Cell hight to 80 Add some pic to asset

---
+++ FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by elliot xin on 8/8/17.
+//  Copyright © 2017 elliot xin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var numeLable: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView:UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
--- FoodPin/FoodPin/RestaurantTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -10,12 +10,52 @@ import UIKit
 
 class RestaurantTableViewController: UITableViewController {
 
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-    
 
 }
 
+
+/*
+ TableView Datasoure
+ 
+ This viewcontroller inheritance from UITableViewController,The defaule UITableViewController already include the datasoure and delegate function, so if you want reuse is, must add override befor the func.and the also no need write UITableViewDatasource and UITableViewDategate in the extension.
+ */
+extension RestaurantTableViewController {
+ 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return restaurantNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
+        
+        cell.numeLable.text = restaurantNames[indexPath.row]
+        cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        cell.thumbnailImageView.layer.cornerRadius = 30.0
+        cell.thumbnailImageView.clipsToBounds = true
+        
+        return cell
+    }
+    
+}
+
+//TableView Delegate
+extension RestaurantTableViewController{
+    
+}
