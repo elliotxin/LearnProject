6d610742f95c5641fee820a09604f00be165a9e2
elliot xin <elliotxin@gmail.com>
2017-08-08 14:27:26 +0800

    initialize project

---
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by elliot xin on 8/8/17.
+//  Copyright © 2017 elliot xin. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
